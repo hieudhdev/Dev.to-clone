aa43c02996617429f07ecf89226e91a994522c74
Hieu <danghieuyt2@gmail.com>
2024-03-28 23:06:51 +0700

    add: notification, post, tag - model

---
+++ Backend/src/models/tag.model.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const tagSchema = new Schema({
+  name: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  posts: [{ type: mongoose.Types.ObjectId, required: true, ref: "Post" }],
+  followers: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+});
+
+module.exports = mongoose.model("Tag", tagSchema); 
