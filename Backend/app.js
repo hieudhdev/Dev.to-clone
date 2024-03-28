e85aab2ef3346d56b507a5cc94ad78bd9c0e34a3
Hieu <danghieuyt2@gmail.com>
2024-03-28 22:59:04 +0700

    add: database connect; Err handle

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+const PORT = process.env.DEV_APP_PORT || 4080
+
+// Connect databases
+require('./src/databases/init.mongodb.js')
 
 app.get('/', (req, res) => {
     res.send('hello')
 })
 
-app.listen(3000, (req, res) => {
-    console.log('listening on')
+// Error handling
+app.use((error, req, res, next) => {
+    const statusCode = error.status || 500
+    return res.status(statusCode).json({
+        status: 'error',
+        code: statusCode,  
+        message: error.message || 'Internal Server Error',
+        stack: error.stack
+    })
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`App listen on port ${PORT}!`)
 })
